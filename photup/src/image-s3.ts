bf3ed1add9e2aa3296dd4184d5244e45720ff044
nag0yan <matsui-ka@kadokawa.jp>
2025-05-25 23:57:06 +0900

    appディレクトリを削除

--- photup/src/image-s3.ts
+++ photup/src/image-s3.ts
@@ -1,11 +1,11 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import type { Create } from "./types/image";
-const bucketName = "photup-images-0123456789";
 const client = new S3Client({
 	region: "ap-northeast-1",
 });
 
 export const create: Create = async (image) => {
+	const bucketName = process.env.BUCKET_NAME
 	await client.send(
 		new PutObjectCommand({
 			Bucket: bucketName,
