3a9110dac6c9dfb0d5e4a0870c2747b4e82d5ea2
nag0yan <matsui-ka@kadokawa.jp>
2025-05-28 23:37:32 +0900

    メタデータをDynamoDBに保存

--- photup/src/image-s3.ts
+++ photup/src/image-s3.ts
@@ -1,18 +1,33 @@
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import type { CreateRequest } from "./types/image";
-const client = new S3Client({
+const s3Client = new S3Client({
+	region: "ap-northeast-1",
+});
+const dynamoDbClient = new DynamoDBClient({
 	region: "ap-northeast-1",
 });
 
 export const create: CreateRequest = async (image) => {
-	const bucketName = process.env.BUCKET_NAME;
 	const decordedData = Buffer.from(image.data, "base64");
-	await client.send(
+	const id = crypto.randomUUID();
+	await s3Client.send(
 		new PutObjectCommand({
-			Bucket: bucketName,
-			Key: crypto.randomUUID(),
+			Bucket: process.env.BUCKET_NAME,
+			Key: id,
 			Body: decordedData,
 			ContentLength: decordedData.byteLength,
 		}),
 	);
+	console.info("Image uploaded to S3 with ID:", id);
+	await dynamoDbClient.send(
+		new PutItemCommand({
+			TableName: process.env.TABLE_NAME,
+			Item: {
+				id: { S: id },
+				filename: { S: image.filename },
+			},
+		}),
+	);
+	console.info("Image metadata saved to DynamoDB with ID:", id);
 };
